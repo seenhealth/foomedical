fee43af271353b2faeb450d983dcd832b11414c7
Yang Su <suyang1+dev@gmail.com>
2023-09-28 09:24:10 -0700

    updated google client id

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Default values work on localhost:3000 and foomedical.com
 // Replace these values with your own values for production
 export const MEDPLUM_PROJECT_ID = 'bc647e64-3d18-485f-8215-e465824c28f5';
-export const MEDPLUM_GOOGLE_CLIENT_ID = '679052511930-8dqur4mmg8egbttgos5pmr4ljtf3etbb.apps.googleusercontent.com';
+export const MEDPLUM_GOOGLE_CLIENT_ID = '824763799228-7b7u4c7uoqrlg2fhpfip3o6q30qkpq5d.apps.googleusercontent.com';
 export const MEDPLUM_RECAPTCHA_SITE_KEY = '6LfFd_8gAAAAAOCVrZQ_aF2CN5b7s91NEYIu5GxL';
